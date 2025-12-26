702c24303ac5dd55948afa61b9fc428346c906e0
Eduardo Alves <Eduardo.lves@outlook.com>
2025-12-26 11:23:12 -0300

    42. Passing and Receiving Props

--- src/index.js
+++ src/index.js
@@ -2,54 +2,54 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-// const pizzaData = [
-//     {
-//         name: "Focaccia",
-//         ingredients: "Bread with italian olive oil and rosemary",
-//         price: 6,
-//         photoName: "pizzas/focaccia.jpg",
-//         soldOut: false,
-//     },
-//     {
-//         name: "Pizza Margherita",
-//         ingredients: "Tomato and mozarella",
-//         price: 10,
-//         photoName: "pizzas/margherita.jpg",
-//         soldOut: false,
-//     },
-//     {
-//         name: "Pizza Spinaci",
-//         ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-//         price: 12,
-//         photoName: "pizzas/spinaci.jpg",
-//         soldOut: false,
-//     },
-//     {
-//         name: "Pizza Funghi",
-//         ingredients: "Tomato, mozarella, mushrooms, and onion",
-//         price: 12,
-//         photoName: "pizzas/funghi.jpg",
-//         soldOut: false,
-//     },
-//     {
-//         name: "Pizza Salamino",
-//         ingredients: "Tomato, mozarella, and pepperoni",
-//         price: 15,
-//         photoName: "pizzas/salamino.jpg",
-//         soldOut: true,
-//     },
-//     {
-//         name: "Pizza Prosciutto",
-//         ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-//         price: 18,
-//         photoName: "pizzas/prosciutto.jpg",
-//         soldOut: false,
-//     },
-// ];
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
 
 function App() {
     return (
-        <div clasName="container">
+        <div className="container">
             <Header />
             <Menu />
             <Footer />
@@ -69,7 +69,8 @@ function Menu() {
     return (
         <main className="menu">
             <h2>Our menu</h2>
-            <Pizza />
+            <Pizza name="Pizza Spinaci" ingredients="Tomato, mozarella, spinach, and ricotta cheese" photoName="pizzas/spinaci.jpg" price={10} />
+            <Pizza name="Pizza Fungi" ingredients="Tomato, mushrooms" photoName="pizzas/funghi.jpg" price={10} />
         </main>
     );
 }
@@ -87,13 +88,18 @@ function Footer() {
     return <footer className="footer">{new Date().toLocaleTimeString()} We're currently open!</footer>;
 }
 
-function Pizza() {
+function Pizza(props) {
+    console.log(props);
+
     return (
-        <divs>
-            <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-            <h3>Pizza</h3>
-            <p>"Tomato, mozarella, spinach, and ricotta cheese"</p>
-        </divs>
+        <div className="pizza">
+            <img src={props.photoName} alt={props.name} />
+            <div>
+                <h3>{props.name}</h3>
+                <p>{props.ingredients}</p>
+                <span>{props.price + 3}</span>
+            </div>
+        </div>
     );
 }
 
